84a2c0cce98f254ac351e16235ed8f5ddc8b64e0
ngoclewill2002 <ngoclewill2002@gmail.com>
2024-02-06 21:38:42 +0700

    123

---
+++ News_Core6/Controllers/Components/MenuTopViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News_Core6.Models;
+
+namespace News_Core6.Controllers.Components
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public MenuTopViewComponent (AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
